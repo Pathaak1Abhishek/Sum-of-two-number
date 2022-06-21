9c7ee168c942980d850e6accc8017e011e969f38
Pathaak1Abhishek <70514012+Pathaak1Abhishek@users.noreply.github.com>
2022-06-21 18:24:45 +0530

    Add files via upload

---
+++ add two number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a,b,c=0;
+   printf("\n Enter first number");
+   scanf("%d",&a);
+   printf("\n Enter secod number");
+   scanf("%d",&b);
+   c=a+b;
+   printf("\n sum of number is %d",c);
+   getch();
+
+}
